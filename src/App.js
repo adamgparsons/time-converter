26648ab66a2a40114149bbc7c9b9f18d03988633
adamgparsons <adamgeorgeparsons@gmail.com>
2019-08-26 08:47:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import TimeZones from "./components/TimeZones";
 
 function App() {
+  const [timeZoneList, setTimeZoneList] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "http://api.timezonedb.com/v2.1/list-time-zone?key=H4DLI5SD6Q65&format=json"
+    )
+      .then(res => res.json())
+      .then(data => {
+        setTimeZoneList(data.zones);
+      })
+      .catch(console.log);
+  }, []);
+
+  timeZoneList.forEach(timeZone => {
+    let zoneName = timeZone.zoneName;
+    zoneName = zoneName.replace(/_/g, " ");
+    const countryName = timeZone.countryName;
+    const slash = zoneName.lastIndexOf("/");
+    const cityOnly = zoneName.substring(slash + 1);
+    timeZone.cityName = cityOnly + ", " + countryName;
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Pick a Timezone</h2>
+      <TimeZones timeZoneList={timeZoneList} />
     </div>
   );
 }
