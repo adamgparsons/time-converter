c649dae04c1ed870e386f5650b1bcfba39027978
Adam Parsons <adamparsons@Adams-MacBook-Air.local>
2019-08-27 10:24:32 +0100

    added 2 timeZones

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import TimeZones from "./components/TimeZones";
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+import theme from "./theme";
 
 function App() {
   const [timeZoneList, setTimeZoneList] = useState([]);
+  const [firstTimeZone, setFirstTimeZone] = useState([]);
+  const [secondTimeZone, setSecondTimeZone] = useState([]);
 
   useEffect(() => {
     fetch(
@@ -25,10 +30,67 @@ function App() {
     timeZone.cityName = cityOnly + ", " + countryName;
   });
 
+  function fetchTimeZone(requestedTimezone, numberSelection) {
+    var myLink =
+      "http://api.timezonedb.com/v2.1/get-time-zone?key=H4DLI5SD6Q65&format=json&by=zone&zone=" +
+      requestedTimezone;
+    fetch(myLink)
+      .then(res => res.json())
+      .then(timeZone => {
+        //formatt the date and time to just time
+        const dateAndTime = timeZone.formatted;
+        timeZone.formattedTime = dateAndTime.substring(
+          dateAndTime.indexOf(" ") + 1
+        );
+        // formatt the location
+        let zoneName = timeZone.zoneName;
+        zoneName = zoneName.replace(/_/g, " ");
+        const countryName = timeZone.countryName;
+        const slash = zoneName.lastIndexOf("/");
+        const cityOnly = zoneName.substring(slash + 1);
+        timeZone.cityName = cityOnly + ", " + countryName;
+        if (numberSelection === "firstSelection") {
+          setFirstTimeZone(timeZone);
+        }
+        if (numberSelection === "secondSelection") setSecondTimeZone(timeZone);
+      });
+  }
+
+  function handleFirstChange(e) {
+    e.preventDefault();
+    fetchTimeZone(e.target.value, "firstSelection");
+  }
+
+  function handleSecondChange(e) {
+    e.preventDefault();
+    fetchTimeZone(e.target.value, "secondSelection");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  console.log(firstTimeZone);
   return (
-    <div className="App">
-      <h2>Pick a Timezone</h2>
-      <TimeZones timeZoneList={timeZoneList} />
+    <div
+      className="App"
+      css={css`
+        padding: ${theme.spacing[6]};
+      `}
+    >
+      <TimeZones
+        selectedTimeZone={firstTimeZone}
+        handleChange={handleFirstChange}
+        handleSubmit={handleSubmit}
+        timeZoneList={timeZoneList}
+      />
+      <br />
+      <TimeZones
+        selectedTimeZone={secondTimeZone}
+        handleChange={handleSecondChange}
+        handleSubmit={handleSubmit}
+        timeZoneList={timeZoneList}
+      />
     </div>
   );
 }
--- src/components/TimeZones.js
+++ src/components/TimeZones.js
@@ -1,57 +1,27 @@
-import React, { useState, useEffect, fr } from "react";
+import React from "react";
 
-function TimeZones({ timeZoneList }) {
-  const [selectedTimeZone, setSelectedTimeZone] = useState("");
-
-  function fetchTime(selectedTimeZone) {
-    var myLink =
-      "http://api.timezonedb.com/v2.1/get-time-zone?key=H4DLI5SD6Q65&format=json&by=zone&zone=" +
-      selectedTimeZone;
-    fetch(myLink)
-      .then(res => res.json())
-      .then(data => {
-        const dateAndTime = data.formatted;
-        const Time = dateAndTime.substring(dateAndTime.indexOf(" ") + 1);
-        setSelectedTimeZone(Time);
-      });
-  }
-  // useEffect(() => {
-  //   var myLink =
-  //     "http://api.timezonedb.com/v2.1/get-time-zone?key=H4DLI5SD6Q65&format=json&by=zone&zone=America/Chicago";
-  //   fetch(myLink)
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       const TimeZoneDate = new Date(data.timestamp * 1000);
-  //       const Time =
-  //         TimeZoneDate.getHours() +
-  //         ":" +
-  //         TimeZoneDate.getMinutes() +
-  //         ":" +
-  //         TimeZoneDate.getSeconds();
-  //       setTestTime(Time);
-  //     })
-  //     .catch(console.log);
-  // }, [setTestTime]);
-
-  function handleChange(e) {
-    fetchTime(e.target.value);
-  }
+function TimeZones({
+  selectedTimeZone,
+  handleChange,
+  handleSubmit,
+  timeZoneList
+}) {
   return (
     <div>
-      <select
-        name="timezones"
-        id="timezones"
-        onChange={handleChange}
-        value={selectedTimeZone.cityName}
-      >
-        <option></option>
-        {timeZoneList.map((timeZone, i) => (
-          <option value={timeZone.zoneName} key={i}>
-            {timeZone.cityName}
-          </option>
-        ))}
-      </select>
-      <h2>{selectedTimeZone}</h2>
+      <h2>Enter a Timezone</h2>
+      <form onSubmit={handleSubmit}>
+        <select name="timezones" id="timezones" onChange={handleChange}>
+          <option />
+          {timeZoneList.map((timeZone, i) => (
+            <option value={timeZone.zoneName} key={i}>
+              {timeZone.cityName}
+            </option>
+          ))}
+        </select>
+        <button>Add time Zone</button>
+      </form>
+      <h3>{selectedTimeZone.cityName}</h3>
+      <input type="text" value={selectedTimeZone.formattedTime} />
     </div>
   );
 }
